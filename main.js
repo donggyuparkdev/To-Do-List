481f3eb1c90b952af033a14be322bc652c63a5bc
DongGyu Park <donggyu.park.dev@gmail.com>
2022-03-17 23:23:07 +0900

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // 2.+ 버튼을 클릭하면 할일이 추가된다
 // 3.delete 버튼을 누르면 할일이 삭제된다
 // 4.check 버튼을 누르면 할일이 끝나면서 밑줄이 간다
+//   1). check 버튼을 클릭하는 순간 true false
+//   2). true이면 끝난걸로 간주하고 밑줄 보여주기
+//   3). false이면 안끝난걸로 간주하고 그대로
 // 5.진행중 끝남 탭을 누르면, 언더바가 이동한다
 // 6.끝남탭은 끝난 아이템만 진행중탭은 진행중인 아이템만
 // 7.전체탭을 누르면 다시 전체 아이템으로 돌아옴
@@ -12,8 +15,58 @@ let taskList = [];
 addButton.addEventListener("click", addTask);
 
 function addTask() {
-    // console.log("clicked");
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false
+    }
+    taskList.push(task);
     console.log(taskList);
+    render();
+}
+
+function render() {
+    let resultHTML = "";
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+        <div class="task-done">${taskList[i].taskContent}</div>
+        <div>
+            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+            <button onclick="deleteTask()">Delete</button>
+        </div>
+    </div>`;
+        } else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask()">Delete</button>
+            </div>
+        </div>`;
+        }
+    }
+
+    document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+
+function toggleComplete(id) {
+    console.log("id:", id);
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+
+function deleteTask() {
+    console.log("삭제하다")
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
